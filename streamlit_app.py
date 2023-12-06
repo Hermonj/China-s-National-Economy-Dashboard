2c02086fd862a18b0041273a3dbf788e33a83bc1
Hermon Assefa <41701546+Hermonj@users.noreply.github.com>
2023-12-06 10:42:23 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
 import streamlit as st
 st.title('China's National Economy Dashboard')
 st.header('The data used in the dashboard is sourced from the file "ChinaNationalEconomyData.csv".')
-st.markdown('The dashboard provides visualizations of key economic indicators for China's national economy. It aims to help users understand the historical trends and current status of the Chinese economy.')
+st.markdown("The dashboard provides visualizations of key economic indicators for China's national economy. It aims to help users understand the historical trends and current status of the Chinese economy.")
