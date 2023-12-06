f00216884ab5e673c1363f7568e3cc01794b9527
Hermon Assefa <41701546+Hermonj@users.noreply.github.com>
2023-12-06 10:47:27 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,10 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+
 st.title("China's National Economy Dashboard")
 st.header("GDP growth, GDP per capita, inflation rate, unemployment rate, and current account balance.")
 st.markdown("The dashboard provides visualizations of key economic indicators for China's national economy. It aims to help users understand the historical trends and current status of the Chinese economy.")
+
+# Read the CSV file
+data = pd.read_csv("ChinaNationalEconomyData.csv")
