e6d32ffb6436db3f1b6785433d1ca01f34b91841
Hermon Assefa <41701546+Hermonj@users.noreply.github.com>
2023-12-11 06:34:54 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,6 +37,11 @@ st.line_chart(data["Implied PPP conversion rate"])
 
 #sidebar
 
+# Using object notation
+add_selectbox = st.sidebar.selectbox(
+    "How would you like to be contacted?",
+    ("Email", "Home phone", "Mobile phone")
+)
 
 # Using "with" notation
 with st.sidebar:
@@ -44,4 +49,5 @@ with st.sidebar:
         "Choose a shipping method",
         ("Standard (5-15 days)", "Express (2-5 days)")
     )
+
 
