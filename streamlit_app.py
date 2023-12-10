82f8e222b46cac53ece752e08724380b8f4b4069
Hermon Assefa <41701546+Hermonj@users.noreply.github.com>
2023-12-11 06:26:44 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,12 +47,5 @@ with st.sidebar:
 
 
 
-#column 02
-col1, col2 = st.columns([3, 1])
-data = np.random.randn(10, 1)
 
-col1.subheader("A wide column with a national chart")
-col1.line_chart(data)
 
-col2.subheader("A narrow column with the national data")
-col2.write(data)
