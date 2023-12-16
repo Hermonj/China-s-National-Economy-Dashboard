a49d89b94e08e01c3505ed8c3027d75eac375301
Hermon Assefa <41701546+Hermonj@users.noreply.github.com>
2023-12-17 05:54:32 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,49 +1,47 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
+import altair as alt
 
+# Page configuration
+st.set_page_config(
+    page_title="China Economic Indicators Dashboard",
+    page_icon="📊",
+    layout="wide",
+    initial_sidebar_state="expanded")
 
+# Load data (example data, replace with actual data)
+df_gdp = pd.DataFrame({
+    'Province': ['Beijing', 'Shanghai', 'Guangdong', 'Zhejiang', 'Jiangsu'],
+    'GDP (in Billion USD)': [600, 650, 1200, 700, 900],
+    'Population (in Million)': [21, 26, 115, 60, 80],
+    'Unemployment Rate (%)': [3.5, 4.2, 4.8, 3.2, 4.5]
+})
 
-st.title("China's National Economy Dashboard")
-st.header(
-    "GDP growth, GDP per capita, inflation rate, unemployment rate, and current account balance 202242260041-ROMHA TEKLE."
-)
-
-st.markdown(
-    "The dashboard provides visualizations of key economic indicators for China's national economy. It aims to help users understand the historical trends and current status of the Chinese economy."
-)
-
-# Read the CSV file
-data = pd.read_csv("Economy_Dashboard2.csv")
-
-# Real GDP Growth
-st.write('This is GDP growth.')
-st.line_chart(data["GDP growth"])
-
-#  GDP per capita
-st.write('This is GDP per capita.')
-st.area_chart(data["GDP per capita"])
-
-# Inflation Ratest.bar_chart(data["Inflation rate"])
-st.write('This is GDP, current prices.')
-st.bar_chart(data["GDP, current prices"])
-
-# Inflation Rate
-st.write('This is GDP based on PPP.')
-st.line_chart(data["GDP based on PPP"])
-
-# Inflation Rate
-st.write('This is Implied PPP conversion rate.')
-st.area_chart(data["Implied PPP conversion rate"])
-
-
-#sidebar
-
-# Using "with" notation
+# Sidebar
 with st.sidebar:
-    add_radio = st.radio(
-        "Choose a method",
-        ("Standard", "Express")
-    )
+    st.title('📊 China Economic Indicators')
 
+# Main Panel
+st.title('Key Economic Indicators')
 
+# GDP Ranking Table
+st.subheader('GDP Ranking by Province')
+st.table(df_gdp[['Province', 'GDP (in Billion USD)']].sort_values(by='GDP (in Billion USD)', ascending=False))
+
+# Line Chart for Population
+st.subheader('Population by Province')
+population_chart = alt.Chart(df_gdp).mark_bar().encode(
+    x='Province',
+    y='Population (in Million)',
+    color=alt.Color('Population (in Million)', scale=alt.Scale(scheme='blues'))
+).properties(width=600, height=400)
+st.altair_chart(population_chart)
+
+# Bar Chart for Unemployment Rate
+st.subheader('Unemployment Rate by Province')
+unemployment_chart = alt.Chart(df_gdp).mark_bar().encode(
+    x='Province',
+    y='Unemployment Rate (%)',
+    color=alt.Color('Unemployment Rate (%)', scale=alt.Scale(scheme='reds'))
+).properties(width=600, height=400)
+st.altair_chart(unemployment_chart)
