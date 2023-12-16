5df2c6b2a757b42d70215dbb9c05e47d2eceece3
Hermon Assefa <41701546+Hermonj@users.noreply.github.com>
2023-12-17 04:44:42 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+
 
 
 st.title("China's National Economy Dashboard")
@@ -26,15 +26,15 @@ st.area_chart(data["GDP per capita"])
 
 # Inflation Ratest.bar_chart(data["Inflation rate"])
 st.write('This is GDP, current prices.')
-st.pyplot(data["GDP, current prices"])
+st.bar_chart(data["GDP, current prices"])
 
 # Inflation Rate
 st.write('This is GDP based on PPP.')
-st.bar_chart(data["GDP based on PPP"])
+st.line_chart(data["GDP based on PPP"])
 
 # Inflation Rate
 st.write('This is Implied PPP conversion rate.')
-st.scatter_chart(data["Implied PPP conversion rate"])
+st.area_chart(data["Implied PPP conversion rate"])
 
 
 #sidebar
